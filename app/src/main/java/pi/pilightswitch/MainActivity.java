9048d879f90aacaae4960247d1ad3ce1f8f92e6b
Your Name <you@example.com>
2019-04-28 15:25:32 -0700

    create function for server task

--- app/src/main/java/pi/pilightswitch/MainActivity.java
+++ app/src/main/java/pi/pilightswitch/MainActivity.java
@@ -2,19 +2,24 @@ package pi.pilightswitch;
 
 import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
+import android.content.Context;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import java.net.SocketException;
+import java.util.concurrent.ExecutionException;
 
+public class MainActivity extends AppCompatActivity {
+    private Context context = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // context is passed to ConnectToServerTask to allow update to ui when exception is caught
+        context = getApplicationContext();
         final ConstraintLayout constraintLayout = (ConstraintLayout) findViewById(R.id.ConstraintLayout);
 
         // fade in to light color when on button is pressed
@@ -27,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
                 new ArgbEvaluator(), getColor(R.color.onColor), getColor(R.color.colorPrimary));
         fadeIn.setDuration(1000);
 
-
         final Button onButton = (Button) findViewById(R.id.onButton);
         final Button offButton = (Button) findViewById(R.id.offButton);
 
@@ -35,18 +39,28 @@ public class MainActivity extends AppCompatActivity {
         onButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new ConnectToServerTask().execute("ON");
-                fadeIn.start();
+                startConnectToServerTask("ON", fadeIn);
             }
         });
-
         // off button
         offButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new ConnectToServerTask().execute("OFF");
-                fadeOut.start();
+                startConnectToServerTask("OFF", fadeOut);
             }
         });
+    }
+    private void startConnectToServerTask(String inputToServer, ObjectAnimator fade) {
+        ConnectToServerTask ct = (ConnectToServerTask) new  ConnectToServerTask(context).execute(inputToServer);
+        try {
+            String asyncConnectionResult = ct.get();
+            // if exception not caught in ConnectToServerTask, allow fade to occur
+            if (asyncConnectionResult == "")
+                fade.start();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
