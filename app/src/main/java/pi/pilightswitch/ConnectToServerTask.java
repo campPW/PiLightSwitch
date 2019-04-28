d82998bcacd00cd25f1a7ae8781759a38ef51195
Your Name <you@example.com>
2019-04-28 15:24:07 -0700

    create toast to display caught exceptions

--- app/src/main/java/pi/pilightswitch/ConnectToServerTask.java
+++ app/src/main/java/pi/pilightswitch/ConnectToServerTask.java
@@ -1,27 +1,55 @@
 package pi.pilightswitch;
+
+import android.content.Context;
 import android.os.AsyncTask;
-import java.io.IOError;
+import android.widget.Toast;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class ConnectToServerTask extends AsyncTask<String,Void, String> {
+        private String connectionResult = "";
+        private Context context;
+
+        public ConnectToServerTask(Context mainContext) {
+            this.context = mainContext;
+        }
      @Override
      protected String doInBackground(String... params) {
          String input = params[0];
          try {
              Socket socket = new Socket("10.0.0.50", 5091);
-
              PrintWriter printWriter = new PrintWriter(socket.getOutputStream(),true);
              printWriter.print(input);
              printWriter.flush();
-
+         }
+         catch (SocketException se) {
+            connectionResult = se.getMessage();
+         }
+         catch (UnknownHostException uhe) {
+             connectionResult = uhe.getMessage();
+         }
+         catch (IOException ioe){
+             System.out.println("INSIDE IO EXCEPTION");
+             connectionResult = ioe.getMessage();
          }
          catch (Exception e) {
-             e.printStackTrace();
+             connectionResult = e.getMessage();
          }
-         catch (IOError ioError){
-             ioError.printStackTrace();
-         }
-         return null;
-         }
+         System.out.println("AFter try catch stuff");
+         return connectionResult;
      }
+    @Override
+    protected void onPostExecute(String result) {
+        super.onPostExecute(result);
+        // display toast containing the exception message
+        if(result != "") {
+            CharSequence text = result;
+            int duration = Toast.LENGTH_LONG;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
+        }
+    }
+}
