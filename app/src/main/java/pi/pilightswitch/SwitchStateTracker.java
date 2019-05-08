eb07d815323f5c4b0cb19c14cc80c8165875ee9e
Your Name <you@example.com>
2019-05-08 11:32:03 -0700

    create constructors and getters/setters

--- app/src/main/java/pi/pilightswitch/SwitchStateTracker.java
+++ app/src/main/java/pi/pilightswitch/SwitchStateTracker.java
@@ -1,4 +1,21 @@
 package pi.pilightswitch;
 
 public class SwitchStateTracker {
+    int state; // 0 = off, 1 = on
+
+    public SwitchStateTracker() {
+        this(0); // default to off
+    }
+    public SwitchStateTracker(int state) {
+        this.setState(state);
+    }
+    public void setState(int state) {
+        if(state != 0 || state != 1)
+            throw new IllegalArgumentException();
+        else
+            this.state = state;
+    }
+    public int getState() {
+        return this.state;
+    }
 }
